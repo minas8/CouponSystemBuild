5b98ea6654eb3e70f77324346cbc14be327892f8
minas8 <minash.8@gmail.com>
2021-07-09 17:35:58 +0300

    Convert to ES6 module

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const path = require("path");
+import express, { static } from "express";
+import { join } from "path";
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
+app.use(static(join(__dirname, "build")));
+app.get("*", (req, res) => {
+    res.sendFile(join(__dirname, "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
