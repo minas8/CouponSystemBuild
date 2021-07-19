f27e430f0b538266a85f311bdf014a28c3ce3b27
minas8 <minash.8@gmail.com>
2021-07-19 12:52:54 +0300

    Fixing server code

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get("*", function (req, res) {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log("App is listening on port " + port);
+// console.log("App is listening on port " + port);
